d28ac9e0370c2c17b6ab1220f9d807d56bf783c8
markusbrammer <git.eapy1@aleeas.com>
2024-02-12 09:47:05 +0100

    Added tuple example

---
+++ examples/Tuple.fsx
@@ -0,0 +1,39 @@
+#r "nuget: Microsoft.Z3, 4.11.2"
+
+open Microsoft.Z3
+
+let ctx = new Context()
+let solver = ctx.MkSolver()
+
+// ----------------------------------------------------------------------------
+// Making tuple sort
+
+let mkTupleSort (name: string) fieldNames fieldSorts =
+    let nameSym: Symbol = ctx.MkSymbol(name)
+
+    let fieldNamesSyms: Symbol array =
+        Array.map (fun (s: string) -> ctx.MkSymbol(s)) fieldNames
+
+    ctx.MkTupleSort(nameSym, fieldNamesSyms, fieldSorts)
+
+let infoSort = mkTupleSort "Info" [| "Name"; "Age" |] [| ctx.StringSort; ctx.IntSort |]
+
+let mkInfo name age = 
+    let args: Expr array = [| ctx.MkString(name); ctx.MkInt(uint64 age) |]
+
+    infoSort.MkDecl.Apply(args)
+
+let getName info = 
+    infoSort.FieldDecls.[0].Apply([| info |])
+
+let getAge info = 
+    infoSort.FieldDecls.[1].Apply([| info |])
+
+// ----------------------------------------------------------------------------
+// Creating/making a tuple and getting field values
+
+let i0 = mkInfo "Markus" 25
+
+(getName i0).Simplify().ToString() // ""Markus""
+(getAge i0).Simplify().ToString() // "25"
+
